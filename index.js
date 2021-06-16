972f0f5f745db8821955a9f59f69715c63c76760
andyvanee <1andyvanee@gmail.com>
2021-06-16 18:49:21 +0000

    Deploying to gh-pages from  @ b3199b3de497fade844c03282c3e40d9bc9f7804 🚀

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ const G=window.ShadowRoot&&(void 0===window.ShadyCSS||window.ShadyCSS.nativeShad
             }
             .input-min-max {
                 display: grid;
-                grid-template-columns: 80px 1fr 80px;
+                grid-template-columns: 1fr 10px 1fr;
                 gap: 0;
             }
             .input-min-max div {
