b5a86109f1fa91407736438db91afcf5a27dfb8f
andyvanee <1andyvanee@gmail.com>
2022-01-03 19:34:39 -0700

    Update to use window.ga(send)

--- src/components/utils.js
+++ src/components/utils.js
@@ -7,6 +7,15 @@
         if (window && window.gtag) {
             window.gtag("event", "tagview", { event_category, event_label })
         }
+        if (window && window.ga) {
+            const eventAction = "tagview"
+            const [eventCategory, eventLabel] = [event_category, eventLabel]
+            window.ga("send", "event", {
+                eventAction,
+                eventCategory,
+                eventLabel
+            })
+        }
         url.searchParams.delete("tid")
         history.pushState({}, document.title, `${url}`)
     }
@@ -22,11 +31,21 @@ export const didNavigate = () => {
             page_path: window.location.pathname
         })
     }
+    if (window && window.ga) {
+        const eventAction = "page_view"
+        window.ga("send", "event", {
+            eventAction,
+            page_title: document.title,
+            page_location: window.location.href,
+            page_path: window.location.pathname
+        })
+    }
 }
 
-export const clickTrack = (category, label) => {
-    if (window && window.dataLayer) {
-        dataLayer.push({ event: "click", category, label })
+export const clickTrack = (eventCategory, eventLabel) => {
+    if (window && window.ga) {
+        const eventAction = "click"
+        window.ga("send", "event", { eventAction, eventCategory, eventLabel })
     }
 }
 
